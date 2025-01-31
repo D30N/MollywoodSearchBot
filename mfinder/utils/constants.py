18e6d2cab8b017004a93d5878521e0cd9e83a30c
d30n <deepakdeon@yahoo.com>
2025-01-31 04:45:54 +0000

    removed button list from help message

--- mfinder/utils/constants.py
+++ mfinder/utils/constants.py
@@ -69,9 +69,5 @@ Result Mode:
 • HyperLink (preferred) ആണെങ്കിൽ link രൂപത്തിലുള്ള result list കിട്ടും.
 • Button ആണെങ്കിൽ ബട്ടൺ രൂപത്തിലാവും result list കിട്ടുക.
 • List ലിസ്റ്റ് ആയി തന്നെ result list കാണിക്കും.
+"""
 
-<<<<<<< HEAD
-താഴെ വലതു വശത്തെ ബട്ടനിൽ ക്ലിക്ക് ചെയ്ത് settings customize ചെയ്യാവുന്നതാണ്."""
-=======
-താഴെ വലതു വശത്തെ ബട്ടനിൽ ക്ലിക്ക് ചെയ്ത് settings customize ചെയ്യാവുന്നതാണ്. """
->>>>>>> 645be2c (Updated serve.py, constants.py, and sample_const.py)
